359531707b4c675d8f30ba66a6e89be329ca468b
Maicon Robert <maiconlk2321@gmail.com>
2022-08-18 22:38:03 -0300

    Dia 04 - Calculadora - Atualização de Estrutura

--- Dia04-Calculadora/Calculadora.py
+++ Dia04-Calculadora/Calculadora.py
@@ -1,8 +1,10 @@
 import flet
-from flet import Page
+from flet import Page, Container, Text, alignment, colors
 
 def main(page : Page):
     page.title = "Calculadora com flet"
+    page.vertical_alignment = page.horizontal_alignment = "center"
+    page.bgcolor = colors.BLACK45
     
     def soma(e):
         page.update()
@@ -17,11 +19,33 @@ def main(page : Page):
         pass
 
     def mult(e):
-        page.update
+        page.update()
         pass
 
+    def porc(e):
+        page.update()
+        pass
 
-    page.add()
+    # Componentes:
+
+    display = ''
+    numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    operadores = ["soma", "sub", "div", "multi", "porc"]
+    caracteres = ["(", ")", ","]
+    buttons = ["igual", "excluir"]
+    
+    page.add(
+        Container(
+            content = Text("Calculadora"),
+            margin = 10,
+            padding = 10,
+            alignment = alignment.center,
+            bgcolor = colors.BLACK87,
+            width = 450,
+            height = 600,
+            border_radius = 10,
+        )
+    )
     page.update()
 
-flet.app(targe=main, view=flet.WEB_BROWSER)
+flet.app(target=main, view=flet.WEB_BROWSER)
