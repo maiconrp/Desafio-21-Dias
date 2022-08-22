e7680a704a2c278e9d58faf20623ba87fe726b2f
Maicon Robert <maiconlk2321@gmail.com>
2022-08-22 04:59:27 -0300

    Dia 07 - Contador - Estrutura lógica, java scriptt

--- Dia07-Contador/script.js
+++ Dia07-Contador/script.js
@@ -1,5 +1,65 @@
-function clique(){
-    document.getElementById("horas").innerHTML = "Clicou"
-    let a = document.getElementById("horas").innerHTML
-    alert(a)
+var contagemSegundos = 1
+var contagemMinutos = 1
+var contagemHoras = 1
+
+var pausa = false
+
+const BTN_PAUSA = document.getElementById("botao_pausa")
+const BTN_INICIAR = document.getElementById("botao_inicio")
+const BTN_REINICIAR = document.getElementById("botao_reinicia")
+
+const SEGUNDOS = document.getElementById("segundos")
+const MINUTOS = document.getElementById("minutos")
+const HORAS = document.getElementById("horas")
+
+function contagem(){
+    if(SEGUNDOS.innerHTML == 59){
+        contagemSegundos = 0
+        MINUTOS.innerHTML = contagemMinutos++
+
+        if (MINUTOS.innerHTML == 59){
+            contagemMinutos = 0
+            HORAS.innerHTML = contagemHoras++
+    }
+}
+    SEGUNDOS.innerHTML = contagemSegundos++
+
+
+}
+
+function Iniciar(){
+    tempo = setInterval(contagem, 1000)
+    BTN_PAUSA.disabled = false
+    BTN_REINICIAR.disabled = false
+  
+}
+
+function Pausar(){ 
+    BTN_PAUSA.innerHTML = BTN_PAUSA.innerHTML == "Pausar" ? "Retomar" : "Pausar";
+    
+    pausa = pausa == false ? true : false
+
+    if(pausa == true){
+        clearInterval(tempo);
+
+    }else{
+        tempo = setInterval(contagem, 1000)
+
+    }
+           
+}
+
+function Reiniciar(){
+    contagemSegundos = 0
+    contagemMinutos = 0
+    contagemHoras = 0
+    
+    SEGUNDOS.innerHTML = contagemSegundos
+    MINUTOS.innerHTML = contagemMinutos
+    HORAS.innerHTML = contagemHoras  
+}
+function test(){
+    document.getElementById("segundos").innerHTML = contagemSegundos
+    document.getElementById("minutos").innerHTML = contagemMinutos
+    document.getElementById("horas").innerHTML = contagemHoras
 }
