38c75abdcab68e9993120ea73b096f4fb0c39bae
Maicon Robert <maiconlk2321@gmail.com>
2022-08-22 15:10:37 -0300

    Dia 07 - Contador - Estrutura lógica, JS, Estilização Final

--- Dia07-Contador/script.js
+++ Dia07-Contador/script.js
@@ -2,64 +2,71 @@ var contagemSegundos = 1
 var contagemMinutos = 1
 var contagemHoras = 1
 
-var pausa = false
+var pausa = true
 
-const BTN_PAUSA = document.getElementById("botao_pausa")
+const BTN_STOP = document.getElementById("botao_stop")
 const BTN_INICIAR = document.getElementById("botao_inicio")
 const BTN_REINICIAR = document.getElementById("botao_reinicia")
+const ICON_PLAY = document.getElementById('icon_play')
 
 const SEGUNDOS = document.getElementById("segundos")
 const MINUTOS = document.getElementById("minutos")
 const HORAS = document.getElementById("horas")
 
 function contagem(){
-    if(SEGUNDOS.innerHTML == 59){
+    if(SEGUNDOS.innerText == 59){
         contagemSegundos = 0
-        MINUTOS.innerHTML = contagemMinutos++
-
-        if (MINUTOS.innerHTML == 59){
+        MINUTOS.innerText = contagemMinutos < 10 ? '0' + contagemMinutos++ : contagemMinutos++
+        if (MINUTOS.innerText == 59){
             contagemMinutos = 0
-            HORAS.innerHTML = contagemHoras++
+            HORAS.innerText = contagemHoras < 10 ? '0' + contagemHoras++ : contagemHoras++
+        }
     }
-}
-    SEGUNDOS.innerHTML = contagemSegundos++
-
-
+    SEGUNDOS.innerText = contagemSegundos < 10 ? '0' + contagemSegundos++ : contagemSegundos++
 }
 
 function Iniciar(){
-    tempo = setInterval(contagem, 1000)
-    BTN_PAUSA.disabled = false
-    BTN_REINICIAR.disabled = false
-  
-}
+    pausa = pausa == true ? false : true
 
-function Pausar(){ 
-    BTN_PAUSA.innerHTML = BTN_PAUSA.innerHTML == "Pausar" ? "Retomar" : "Pausar";
-    
-    pausa = pausa == false ? true : false
+    BTN_STOP.disabled = false
+    BTN_REINICIAR.disabled = false
+
+    let class_pausa = ICON_PLAY.className
+    ICON_PLAY.className = class_pausa == "icofont-ui-pause" ? "icofont-ui-play" : "icofont-ui-pause"
 
     if(pausa == true){
         clearInterval(tempo);
-
     }else{
         tempo = setInterval(contagem, 1000)
-
     }
+    
+  
+}
+
+function Stop(){ 
+    Reiniciar()
+    clearInterval(tempo);
+
+    pausa = true
+
+    BTN_STOP.disabled = true
+    BTN_REINICIAR.disabled = true
+
+    ICON_PLAY.className = "icofont-ui-play"
            
 }
 
 function Reiniciar(){
-    contagemSegundos = 0
-    contagemMinutos = 0
-    contagemHoras = 0
+    contagemSegundos = '0' + 0
+    contagemMinutos = '0' + 0
+    contagemHoras = '0' + 0
     
-    SEGUNDOS.innerHTML = contagemSegundos
-    MINUTOS.innerHTML = contagemMinutos
-    HORAS.innerHTML = contagemHoras  
+    SEGUNDOS.innerText = contagemSegundos
+    MINUTOS.innerText = contagemMinutos
+    HORAS.innerText = contagemHoras  
 }
 function test(){
-    document.getElementById("segundos").innerHTML = contagemSegundos
-    document.getElementById("minutos").innerHTML = contagemMinutos
-    document.getElementById("horas").innerHTML = contagemHoras
+    document.getElementById("segundos").innerText = contagemSegundos
+    document.getElementById("minutos").innerText = contagemMinutos
+    document.getElementById("horas").innerText = contagemHoras
 }
