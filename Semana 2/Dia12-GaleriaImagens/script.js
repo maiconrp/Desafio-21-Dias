b9ccbd56cd5f03ae23ff525afc71513780e0939e
Maicon Robert <maiconlk2321@gmail.com>
2022-08-29 22:06:02 -0300

    Ajustes de largura css e rascunhos js

---
+++ Semana 2/Dia12-GaleriaImagens/script.js
@@ -0,0 +1,15 @@
+// var fotoID = document.getElementById("foto0")
+// // var fotoClas = document.querySelector(".imgFoto")
+// // console.log(foto)
+
+// function preview(elemento){
+//     document.body.style.background = "#fff"
+//     fotoID.style.alignSelf="center"
+//     fotoID.style.position = "fixed"
+//     fotoID.style.top = "50vh-height"
+//     fotoID.style.left = "50vw-width"
+// }
+// fotoID.addEventListener("click", preview, false);
+
+
+
